910f07a48c6ce2e2d01dc56af6cab0f59dd605b1
Nicolas Lépinay <nicolas@nosoft.io>
2023-11-30 15:52:06 +0100

    Restore test result to test Action success

--- __tests__/computes.js
+++ __tests__/computes.js
@@ -3,7 +3,7 @@ import ComputeService from '../src/services/compute';
 describe('ComputeService', () => {
   it('should add two numbers', () => {
     const added = ComputeService.Add(1, 2);
-    expect(added).toBe(999999999999999);
+    expect(added).toBe(3);
   });
   it('should add multiple numbers', () => {
     const added = ComputeService.Add(1, 2, 3);
